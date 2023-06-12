fec9dc7a88aa8927170d00509512a2d81f2bd2aa
John Kennedy Haringa Peña <jkhp.jkpa@gmail.com>
2023-06-12 16:43:33 +0800

    position: absolute;
    css test

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 var style = document.createElement("style");
-style.innerHTML = "#particles-js { width: 100%; height: 100%; position: fixed; } body { background-color: black; color: white; }";
+style.innerHTML = `
+#particles-js { 
+    width: 100%; 
+    height: 100%; 
+    position: fixed; 
+} 
+.container-lg px-3 my-5 markdown-body {
+    position: absolute;
+}
+body { 
+    background-color: black; 
+    color: white; 
+}`;
 document.head.appendChild(style);
 
 var particlesDiv = document.createElement("div");
 particlesDiv.setAttribute("id", "particles-js");
-document.body.insertBefore(particlesDiv, document.body.firstChild);
+document.body.appendChild(particlesDiv);
 
 var particlesScript = document.createElement("script");
 particlesScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js");
-document.body.insertBefore(particlesScript, document.body.firstChild);
+document.body.appendChild(particlesScript);
 
 var configScript = document.createElement("script");
 configScript.setAttribute("src", "https://gdjkhp.github.io/meeseeks-leaderboard-api/particlesjs-config.js");
-document.body.insertBefore(configScript, document.body.firstChild);
+document.body.appendChild(configScript);
