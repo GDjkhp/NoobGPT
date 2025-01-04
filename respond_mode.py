b578a6c071a3d1f3e47df259d6ea90e68598ebdb
GDjkhp <jkhp.jkpa@gmail.com>
2025-01-04 09:24:19 +0800

    aimode: add random message rate test
    gg it avenue

--- respond_mode.py
+++ respond_mode.py
@@ -1,8 +1,8 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
-from util_discord import command_check, check_if_master_or_admin
-from util_database import set_ai_mode, get_database2
+from util_discord import command_check, check_if_master_or_admin, description_helper
+from util_database import set_ai_mode, get_database2, set_ai_rate
 from googleai import models_google, GEMINI_REST
 from perplexity import models_mistral, models_groq, models_github, main_mistral, main_groq, main_github
 models_master = models_google + models_mistral + models_groq + models_github + ["off"]
@@ -12,10 +12,14 @@ async def ai_respond_mode(ctx: commands.Context, model: str):
     if not await check_if_master_or_admin(ctx): return await ctx.reply("not a bot master or an admin")
     id = ctx.guild.id if ctx.guild else ctx.channel.id
     db = await get_database2(id) # please be good
-    if not model in models_master: return await ctx.reply(f"model not found\n{models_master}")
-    await set_ai_mode(id, model)
-    if model != "off": await ctx.reply(f"{model} has been set as my default response mode. talk to me and see what happens.")
-    else: await ctx.reply(f"ai response mode has been disabled. talk to me and i'll roast you instead.")
+
+    if not model in models_master: return await ctx.reply(f"Model not found.\nAvailable models\n```{' '.join(models_master)}```")
+    if model != "off":
+        await set_ai_mode(id, model)
+        await ctx.reply(f"{model} has been set as my default response mode. talk to me and see what happens.")
+    else:
+        await set_ai_mode(id, "") # tsundere mode
+        await ctx.reply(f"ai response mode has been disabled. talk to me and i'll roast you instead.")
 
 async def ted_talk_response(ctx: commands.Context, model):
     async with ctx.typing(): # users and discord itself will hate me for this
@@ -27,6 +31,23 @@ async def ted_talk_response(ctx: commands.Context, model):
             await main_groq(ctx, model, debug=False)
         if model in models_github:
             await main_github(ctx, model, debug=False)
+            
+async def ai_respond_rate(ctx: commands.Context, rate: str):
+    if await command_check(ctx, "aimode", "utils"): return
+    if not await check_if_master_or_admin(ctx): return await ctx.reply("not a bot master or an admin")
+    id = ctx.guild.id if ctx.guild else ctx.channel.id
+    db = await get_database2(id) # please be good
+    
+    if not rate.isdigit(): return await ctx.reply("not a digit :(")
+    rate = fix_num(rate)
+    await set_ai_rate(id, rate)
+    await ctx.reply(f"ai response mode rate is now set to `{rate}%`")
+    
+def fix_num(num):
+    num = int(num)
+    if num < 0: num = 0
+    elif num > 100: num = 100
+    return num
 
 async def model_auto(interaction: discord.Interaction, current: str) -> list[app_commands.Choice[str]]:
     return [
@@ -37,13 +58,20 @@ class AIModeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.hybrid_command(description="set ai mode description placeholder")
+    @commands.hybrid_command(description=f"{description_helper['emojis']['utils']} {description_helper['utils']['aimode']}")
     @app_commands.describe(model="Large language model")
     @app_commands.autocomplete(model=model_auto)
     @app_commands.allowed_installs(guilds=True, users=True)
     @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
     async def aimode(self, ctx: commands.Context, model: str=None):
         await ai_respond_mode(ctx, model)
+        
+    @commands.hybrid_command(description=f"{description_helper['emojis']['utils']} Set AI mode message rate")
+    @app_commands.describe(rate="Random message rate")
+    @app_commands.allowed_installs(guilds=True, users=True)
+    @app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+    async def airate(self, ctx: commands.Context, rate: str="1"):
+        await ai_respond_rate(ctx, rate)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(AIModeCog(bot))
