1ad2bbc84c83008b74958ac6204974f8df40e02b
GDjkhp <jkhp.jkpa@gmail.com>
2025-01-07 14:14:51 +0800

    HEADER

--- respond_mode.py
+++ respond_mode.py
@@ -29,7 +29,7 @@ async def ai_respond_mode(ctx: commands.Context, model: str):
     db = await get_database2(id) # please be good
 
     model_collect = '\n* '.join(models_master)
-    if not model in models_master: return await ctx.reply(f"Available models:\n```md\n* {model_collect}```")
+    if not model in models_master: return await ctx.reply(f"# Available models\n```md\n* {model_collect}```")
     if model != "off":
         await set_ai_mode(id, model)
         await ctx.reply(f"`{model}` has been set as my default response mode. talk to me and see what happens.")
