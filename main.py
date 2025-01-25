7ba4a2f4e305558dd59cbcef645e4c5ac81ff274
GDjkhp <jkhp.jkpa@gmail.com>
2025-01-25 19:00:35 +0800

    ty mystypy

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import discord
 from discord.ext import commands
 import wavelink
 import sys
+import secrets
 
 discord.utils.setup_logging()
 intents = discord.Intents.default()
@@ -44,6 +45,7 @@ class NoobGPT(commands.Bot):
                          help_command = None, allowed_mentions = mentions)
         self.token = os.getenv(token)
         self.identifier = token
+        self.node_id = secrets.token_urlsafe(12)
 
     async def on_ready(self):
         print(f"{self.identifier} (c) {datetime.now().year} The Karakters Kompany. All rights reserved.")
@@ -92,6 +94,7 @@ class Moosic(commands.Bot):
                          help_command = None, allowed_mentions = mentions)
         self.token = os.getenv(token)
         self.identifier = token
+        self.node_id = secrets.token_urlsafe(12)
         self.bots = bots
 
     async def on_ready(self):
