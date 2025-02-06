be7a4ad55db35873c37b6fc500a52d64802195de
GDjkhp <jkhp.jkpa@gmail.com>
2025-02-06 13:55:58 +0800

    no id no entry

--- music.py
+++ music.py
@@ -273,7 +273,7 @@ class MusicUtil(commands.Cog):
     @commands.command(name="mreset")
     async def reset(self, ctx: commands.Context):
         if check_if_not_owner(ctx): return
-        n: wavelink.Node = wavelink.Pool.get_node(identifier=self.bot.node_id)
+        n = wavelink.Pool.get_node(self.bot.node_id)
         if n: await n.close(eject=True)
         await setup_hook_music(self.bot)
 
