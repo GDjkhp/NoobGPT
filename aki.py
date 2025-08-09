d8060aba415193733064db810df4d8a4ced122e9
GDjkhp <jkhp.jkpa@gmail.com>
2025-08-10 07:07:18 +0800

    aki experiment

--- aki.py
+++ aki.py
@@ -100,7 +100,7 @@ class ButtonAction(discord.ui.Button):
                     embed_loss = create_loss_embed(self.ctx)
                     return await interaction.edit_original_response(embed=embed_loss, view=None)
                 await self.aki.answer(self.action)
-                if self.aki.id_proposition: # see aki_new.py _update_from_answer func win condition
+                if self.aki.progression > 95 and self.aki.win:
                     embed = create_win_embed(self.ctx, self.aki)
                     return await interaction.edit_original_response(
                         embed=embed,
@@ -137,7 +137,6 @@ class ResultButton(discord.ui.Button):
             )
         try:
             if self.action == 'y':
-                await self.aki.choose()
                 embed_win = create_final_embed(self.ctx, self.aki)
                 await interaction.response.edit_message(embed=embed_win, view=None)
             else:
