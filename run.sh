a6763a0cee47969eab8eeaa2ed8cf10252042614
GDjkhp <jkhp.jkpa@gmail.com>
2026-04-06 16:10:15 +0800

    target jsc

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 pip install -U --prefix .local -r requirements.txt
-pip install -U --prefix .local ytdlp-jsc --only-binary=:all:
+pip install -U --prefix .local ytdlp-jsc --only-binary=:all: --target ~/.yt-dlp/plugins/
 /usr/local/bin/python /home/container/main.py
