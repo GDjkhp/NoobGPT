bbad3170d87c8ea8472f513d0c7a60d5f4bf7630
GDjkhp <jkhp.jkpa@gmail.com>
2026-04-06 16:34:44 +0800

    jsc import test

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 pip install -U --prefix .local -r requirements.txt
-pip install -U --target ~/.yt-dlp/plugins/ ytdlp-jsc --only-binary=:all: 
+pip install -U --prefix .local ytdlp-jsc --only-binary=:all:
 /usr/local/bin/python /home/container/main.py
